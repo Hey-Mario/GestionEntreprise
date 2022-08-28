71b88153820ec8d101cf79e808954279782a95f7
Mario Lovamanitra <lovamanitra.randriamanantena@esti.mg>
2022-08-28 09:32:04 +0300

    Add a calendar

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from flask import *
 from werkzeug.utils import secure_filename
+import calendar
+
+calendar = calendar.HTMLCalendar(firstweekday=0).formatmonth(2022, 8)
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return render_template("index.html", calendar=calendar)
 
 
 if __name__ == "__main__":
